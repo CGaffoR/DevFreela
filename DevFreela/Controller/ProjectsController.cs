a791604972ea4db850e118197360660466f78c01
Carlos Gaffo <carlos.gaffo@live.com>
2023-10-06 11:54:33 -0300

    Adicionar arquivos de projeto.

---
+++ DevFreela/Controller/ProjectsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
